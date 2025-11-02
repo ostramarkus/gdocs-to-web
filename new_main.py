3c8fd391fc7760586f9cfda0d3c456be03313fc5
Markus Pettersson <markus.pettersson@edu.huddinge.se>
2025-11-02 17:15:16 +0100

    Refactoring to OOP

--- new_main.py
+++ new_main.py
@@ -6,14 +6,15 @@ def main():
     with open('site-config.yaml', encoding="utf-8") as config_file:
         site_config = yaml.safe_load(config_file.read())
 
+    # Create navigation data
+    navigation = [(page['id'], page['link_text'], page['html_file']) for page in site_config]
+
+    # Iterate each page - process and save
     for page in site_config:
         md_content_file = 'md/' + page['md_file']
         html_template_file = 'templates/main.html'
         title = page['title']
 
-        # Create navigation data
-        navigation = [(page['id'], page['link_text'], page['html_file']) for page in site_config]
-        testpage = md2html.Document('md/index.md', 'templates/main.html', 'Testpage')
         # testpage.insert_main_nav(navigation)
         
         # print(testpage.content_soup)
